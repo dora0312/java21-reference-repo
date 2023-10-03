4b4b2ba17539e666a6c17ffefdc8d0ae3419ff7a
ador0312 <dor.a032294@gmail.com>
2023-10-03 19:29:06 +0700

    lesson04-test02 update

--- lesson04-ador/src/homework/test02/Test02App.java
+++ lesson04-ador/src/homework/test02/Test02App.java
@@ -13,12 +13,19 @@ public class Test02App {
 			index = Integer.parseInt(sc.nextLine());
 		}while(index < 1 || index > 20);
 		sc.close();
-
+		
+		//index = 11;
+//		int[] array = new int[] {12, 21, 25, 19, 32, 35, 49, 10, 33, 14, 18};
 		int[] array = new int[index];
+		
 		for(int i = 0; i < index; i ++) {
 			array[i] = new Random().nextInt(4, 101);			
 		}
+		
 		System.out.println("Check Before: " + Arrays.toString(array));
+		for(int num = 0; num < array.length; num++) {
+			System.out.println(array[num]+"/7 = " + (array[num]%7 == 0) + "; /5 = " + (array[num]%5 == 0));
+		}
 		checkArray(array);
 		System.out.println("Check after: " + Arrays.toString(array));
 	}
@@ -26,17 +33,19 @@ public class Test02App {
 		int temp = 0;
 		int indexLeft = 0;
 		int indexRight = array.length - 1;
-		for(int i = 0; i < indexRight; i++) {
+		for(int i = 0; i <= indexRight; i++) {
 			if( (array[i] % 7 == 0) && (array[i] % 5 != 0) ) {
 				temp = array[indexLeft];
 				array[indexLeft] = array[i];
 				array[i] = temp;
 				indexLeft++;
+				i--;
 			} else if( (array[i] % 5 == 0) && (array[i] % 7 != 0) ) {
 				temp = array[indexRight];
 				array[indexRight] = array[i];
 				array[i] = temp;
 				indexRight--;
+				i--;
 			}
 		}
 	}
